b5bbe7ab9898f24d696745c062479abfeed1b727
goltermann <goltermann@google.com>
2016-02-24 16:37:23 -0800

    Rearrange nodejs install earlier.

--- gce_setup.sh
+++ gce_setup.sh
@@ -6,13 +6,15 @@
 #   sudo apt-get -y install git
 #   git clone https://github.com/goltermann/github-dashing.git
 #   cd github-dashing
+# update .env file with GitHub user and OAUTH read repo token
 # run this script
-#   ./gce.sh
+#   ./gce_setup.sh
+# access at <static_ip>:3030
 sudo apt-get -y install rubygems
 sudo apt-get -y install ruby-dev
 sudo apt-get -y install build-essential
+sudo apt-get -y install nodejs
 sudo gem install dashing
 sudo gem install bundler
 bundle install
-sudo apt-get -y install nodejs
 sudo dashing start
